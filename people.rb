faf197f609660d7294a737250e47a529b08bc109
ndoak <natalieannedoak@gmail.com>
2016-01-14 12:51:51 -0500

    Create an instance of instructor called Chris

--- people.rb
+++ people.rb
@@ -1,11 +1,27 @@
-class Student
+class Person
+  attr_accessor : name
+
+  def initialize(first_name)
+    @first_name = first_name
+  end
+
+  def greeting
+    "Hi, my name is #{name}"
+  end
+
+class Student < Person
   def learn
     "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in Ruby is an Object"
   end
+end
+
+chris = Student.new
+chris.greeting
+
 end
